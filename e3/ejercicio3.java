52494d742c61d1cc1f0572c414630ff8bbc04a0c
AFF40 <101578368+AFF40@users.noreply.github.com>
2022-06-20 10:39:06 -0400

    Add files via upload

---
+++ e3/ejercicio3.java
@@ -0,0 +1,16 @@
+package e3;
+public class ejercicio3 {
+    public static int metodo1(int []vector) {
+        int sumavector = 0;
+        int contador = 0;
+
+        if (contador == 0 || contador != 0) {
+
+            contador++;
+            sumavector = sumavector + vector[contador];
+            System.out.println(sumavector);
+        }
+        return sumavector;
+
+    }
+    }
---
+++ e3/inicio.java
@@ -0,0 +1,11 @@
+import e3.ejercicio3;
+public class inicio {
+    public static void main(String[] args) {
+        System.out.println(ejercicio3.metodo1(new int[0]));
+        System.out.println(ejercicio3.metodo1(new int[1]));
+        System.out.println(ejercicio3.metodo1(new int[2]));
+        System.out.println(ejercicio3.metodo1(new int[3]));
+        System.out.println(ejercicio3.metodo1(new int[4]));
+        System.out.println(ejercicio3.metodo1(new int[5]));
+    }
+}
